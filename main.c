ae4162969c397536157fc0a32710981542ea662d
DariyaDro <116110019+DariyaDro@users.noreply.github.com>
2022-11-04 12:18:09 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,55 +1,38 @@
-
-/** @file
- *
- * @defgroup blinky_example_main main.c
- * @{
- * @ingroup blinky_example
- * @brief Blinky Example Application main file.
- *
- * This file contains the source code for a sample application to blink LEDs.
- *
- */
-
 #include <stdbool.h>
 #include <stdint.h>
 #include "nrf_delay.h"
 #include "boards.h"
-#define PAUSE 5000
-#define SMALL_PAUSE 500
-
-/**
- * @brief Function for application main entry.
- */
+#include "control_led.h"
+#include "control_sw.h"
+#define PAUSE_MS 5000
+#define SMALL_PAUSE_MS 500
+#define ARRAY_SIZE_OF_BLINK 4
 int main(void)
-{
-    /* Configure board. */
-    bsp_board_init(BSP_INIT_LEDS);
-    
-
-    /* Toggle LEDs. */
-   while (true)
-    {
-        for(int j=0; j<LEDS_NUMBER; j++){
-            for(int i=0; i<6*2; i++){
-                bsp_board_led_invert(j);
-                nrf_delay_ms(SMALL_PAUSE);
+{   
+    leds_init();
+    leds_off();
+    switch_init();
+    // 0 - Yellow
+    // 1 - Red
+    // 2 - Green
+    // 3 - Blue
+    int arr[2][ARRAY_SIZE_OF_BLINK]={{6,5,7,9}, {0, 1, 2, 3}}; // вторая строка для расположения светодиодов в нужном порядке
+    while(true){                                               
+        for(int j=0; j<ARRAY_SIZE_OF_BLINK; j++){
+            for(int k=0; k<arr[0][j]; k++){
+                int number_of_led=arr[1][j];
+                turn_on_led(number_of_led);
+                nrf_delay_ms(SMALL_PAUSE_MS);
+                while(!is_switch_press()){
+                    nrf_delay_ms(100);
+                }
+                turn_off_led(number_of_led);
+                nrf_delay_ms(SMALL_PAUSE_MS);
+                while(!is_switch_press()){
+                    nrf_delay_ms(100);
+                }
+                leds_off();
             }
-            nrf_delay_ms(PAUSE);
-            for(int i=0; i<5*2; i++){
-                bsp_board_led_invert(j);
-                nrf_delay_ms(SMALL_PAUSE);
-            }
-            nrf_delay_ms(PAUSE);
-            for(int i=0; i<7*2; i++){
-                bsp_board_led_invert(j);
-                nrf_delay_ms(SMALL_PAUSE);
-            }
-            nrf_delay_ms(PAUSE);
-            for(int i=0; i<9*2; i++){
-                bsp_board_led_invert(j);
-                nrf_delay_ms(SMALL_PAUSE);
-            }
-            nrf_delay_ms(PAUSE);
         }
-   }
-}
+    }
+}      
